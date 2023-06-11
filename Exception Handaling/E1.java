cb4a6b7d50c08cdb4d61396b47a632c73f9b840c
Vinay <vinaypal1220@gmail.com>
2023-06-11 16:35:16 +0530

    exception edit

--- Exception Handaling/E1.java
+++ Exception Handaling/E1.java
@@ -13,8 +13,8 @@
 // }
 class E1{
     public static void main(String[] args){
-        int a = 2;
-        int b = 7;
+        int a = 2; 
+         int b = 7;
         int c = a+b;
         System.out.println(c);
 
